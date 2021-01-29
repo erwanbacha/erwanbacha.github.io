e5d7adefd14622abb6a9a4523fa9b815eaff484b
ChuckHalmond <charles.mecheriki@gmail.com>
2021-01-29 15:54:22 +0100

    Fixes + meta description

--- src/js/data.js
+++ src/js/data.js
@@ -335,15 +335,15 @@ const data = {
 					
 					[2.jpg]
 					
-					[3.jpg][4.jpg]
+					[3.jpg]
 
+					[4.jpg]
+					
 					[5.jpg]
-					
-					[6.jpg]
 
-					[7.jpg][8.jpg]
+					[6.jpg]
 					
-					[9.jpg]`
+					[7.jpg]`
 			},
 			Mum : {
 				title : "MUM",
@@ -506,7 +506,7 @@ const data = {
 
 					[9.jpg]
 					
-					[cover1.jpg][11.jpg]
+					[10.jpg][11.jpg]
 
 					[12.jpg][13.jpg]
 
